bb450c9686ab36eff7edb4c5771af828a7ef546f
Rodrigo <rodribola.0886@gmail.com>
2022-12-03 11:04:39 +0000

    Login update

--- src/main/java/pt/ipvc/rastreio/sistemaderastreio/backend/user.java
+++ src/main/java/pt/ipvc/rastreio/sistemaderastreio/backend/user.java
@@ -1,31 +1,86 @@
 package pt.ipvc.rastreio.sistemaderastreio.backend;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class user {
+    public static ArrayList<user> users = new ArrayList<user>();
+    private int id;
     private String name;
     private String username;
     private String password;
     private int numberPhone;
     private int numOfWork;
 
-    public user(){}
+    public user(String name, String username, String password, int numberPhone){
+        this.id += 1;
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.numberPhone = numberPhone;
+    }
+
+    public void setUsers(ArrayList<user> users) {
+        this.users = users;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
     }
 
     public void setUsername(String username) {
         this.username = username;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public int getNumberPhone() {
+        return numberPhone;
     }
 
     public void setNumberPhone(int numberPhone) {
         this.numberPhone = numberPhone;
     }
 
+    public int getNumOfWork() {
+        return numOfWork;
+    }
+
     public void setNumOfWork(int numOfWork) {
         this.numOfWork = numOfWork;
+    }
+
+    @Override
+    public String toString() {
+        return "user{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", numberPhone=" + numberPhone +
+                ", numOfWork=" + numOfWork +
+                '}';
     }
 }
--- src/main/java/pt/ipvc/rastreio/sistemaderastreio/controller/UserController.java
+++ src/main/java/pt/ipvc/rastreio/sistemaderastreio/controller/UserController.java
@@ -1,14 +1,45 @@
 package pt.ipvc.rastreio.sistemaderastreio.controller;
+import pt.ipvc.rastreio.sistemaderastreio.backend.*;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import static pt.ipvc.rastreio.sistemaderastreio.backend.user.*;
 
 public class UserController {
     @FXML
-    private Label welcomeText;
-
+    private TextField userNameLogin;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private PasswordField passwordLogin;
+    @FXML
+    private TextField userNameRegister;
+    @FXML
+    private PasswordField passwordRegister;
+    @FXML
+    private PasswordField confirmPasswordRegister;
+    @FXML
+    private TextField nameRegister;
+    @FXML
+    private TextField phoneRegister;
+    @FXML
+    private Button buttonLogin;
+    @FXML
+    private Button buttonRegister;
+
+    public void buttonRegisterAction(){
+        user newUser = new user(nameRegister.getText(), nameRegister.getText(), passwordRegister.getText(), Integer.parseInt(phoneRegister.getText()));
+        users.add(newUser);
     }
+
+    public void buttonLoginAction(){
+        for (user u: users){
+            if (u.getUsername().equals(userNameLogin.getText()) && u.getPassword().equals(passwordLogin.getText())){
+                buttonLogin.setText("login com sucesso");
+            }
+        }
+    }
+
 }
