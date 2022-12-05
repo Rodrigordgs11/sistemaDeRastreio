a4f2669dbc81b4b5f4a3834b2654e13afaa06795
Rodrigo <rodribola.0886@gmail.com>
2022-12-05 10:17:10 +0000

    upadate admin file

--- src/main/java/pt/ipvc/rastreio/sistemaderastreio/backend/admin.java
+++ src/main/java/pt/ipvc/rastreio/sistemaderastreio/backend/admin.java
@@ -1,4 +1,22 @@
 package pt.ipvc.rastreio.sistemaderastreio.backend;
+import pt.ipvc.rastreio.sistemaderastreio.backend.user;
 
-public class admin {
+public class admin extends user{
+    int idAdmin;
+
+    public admin(String name, String username, String password, int idAdmin) {
+        super(name, username, password);
+        this.idAdmin = idAdmin;
+    }
+
+    public int getIdAdmin() {
+        return idAdmin;
+    }
+
+    @Override
+    public String toString() {
+        return "admin{" +
+                "idAdmin=" + idAdmin +
+                '}';
+    }
 }
